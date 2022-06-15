e8832aa0b33c3b6cf9574c05144684f4b443ffb9
ElJonathanM <2002dominguezhernandez@gmail.com>
2022-06-14 20:47:44 -0600

    Cambio del dia 14/06/2022 Tomar Foto Y Permisos

--- app/src/main/java/com/example/pm012pp1/ActivityPhoto.java
+++ app/src/main/java/com/example/pm012pp1/ActivityPhoto.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -68,14 +69,24 @@ public class ActivityPhoto extends AppCompatActivity {
         if (requestCode == REQUESTCODECAMARA){
             if (grantResult.length > 0 && grantResult[0] == PackageManager.PERMISSION_GRANTED){
                 TomarFotografia();
+
             }else{
                 Toast.makeText(getApplicationContext(), "Permiso Denegado", Toast.LENGTH_SHORT).show();
+
             }
         }
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @NonNull Intent data){
         super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUESTTAKEPHOTO && resultCode == RESULT_OK){
+            Bundle extraerfoto = data.getExtras();
+            Bitmap imagenBitmap = (Bitmap) extraerfoto.get("data");
+            ObjectImage.setImageBitmap(imagenBitmap);
+
+        }
+
     }
 
 }
